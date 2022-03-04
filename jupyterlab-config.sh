f0d41e5bbbbab93da3299e9646e787a708320bf8
jpscriptdepot <jpgv2004a1@outlook.com.br>
2022-03-03 21:15:41 -0600

    Updating jupyterlab scripts

--- jupyterlab-config.sh
+++ jupyterlab-config.sh
@@ -8,7 +8,7 @@ sed -i -e '1ic.ServerApp.root_dir = "/home/jupyterlab/"\' /home/jupyterlab/.jupy
 sed -i -e '1ic.ServerApp.ip = "*"\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
 sed -i -e '1ic.ExtensionApp.open_browser = False\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
 sed -i -e '1ic.ServerApp.port = 8001\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
-sed -i -e '1ic.ServerApp.base_url = '/jupyterlab'\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ServerApp.base_url = \"/jupyterlab\"' /home/jupyterlab/.jupyter/jupyter_lab_config.py
 
 jupyter-lab password
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout jupyter.key -out jupyter.pem

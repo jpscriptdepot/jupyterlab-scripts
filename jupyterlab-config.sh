e7e6854457ddaaae66207487fbc3803aef9bf9c2
jpscriptdepot <jpgv2004a1@outlook.com.br>
2022-03-03 20:54:51 -0600

    Updating jupyterlab scripts

--- jupyterlab-config.sh
+++ jupyterlab-config.sh
@@ -1,12 +1,14 @@
+cd /home/jupyterlab
+
 pip3 install --upgrade pip
 pip3 install jupyterlab
 
 jupyter-lab --generate-config
-sed -i -e '1ic.ServerApp.root_dir = "/home/jupyter/"\' /home/jupyter/.jupyter/jupyter_lab_config.py
-sed -i -e '1ic.ServerApp.ip = "*"\' /home/jupyter/.jupyter/jupyter_lab_config.py
-sed -i -e '1ic.ExtensionApp.open_browser = False\' /home/jupyter/.jupyter/jupyter_lab_config.py
-sed -i -e '1ic.ServerApp.port = 8001\' /home/jupyter/.jupyter/jupyter_lab_config.py
-sed -i -e '1ic.ServerApp.base_url = '/jupyterlab'\' /home/jupyter/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ServerApp.root_dir = "/home/jupyterlab/"\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ServerApp.ip = "*"\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ExtensionApp.open_browser = False\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ServerApp.port = 8001\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
+sed -i -e '1ic.ServerApp.base_url = '/jupyterlab'\' /home/jupyterlab/.jupyter/jupyter_lab_config.py
 
 jupyter-lab password
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout jupyter.key -out jupyter.pem
